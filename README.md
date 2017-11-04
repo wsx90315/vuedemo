5524963c9c55e085fb3d71abd302d83b6a95e234
moshina <wsx90315@gmail.com>
2017-11-04 21:16:29 +0800

    vue0916

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # vuedemo
+
+```sudo npm install vue-cli -g 權限全域安裝 vue```
+```vue init webpack-simple starbase（資料夾名稱） 生一個vue webpack工作包```
+```進到 starbase 執行npm install```
+```執行 npm run dev```
---
+++ applications/starbase/src/App.vue
@@ -0,0 +1,27 @@
+<<template>
+<div id="app">
+    <h3>{{title}}</h3>
+    <div class="col-md-12">
+        <character 
+        v-for="(id,index) in initial_ids"
+        :id="1" 
+        key="index"
+        />
+    </div>
+</div>
+</template>
+<<script>
+import Character from './components/Character.vue'
+export default{
+    name: 'app',
+    data(){
+        return {
+            title: 'Genearate Your Team',
+            initial_ids:[1,13,14]
+        }
+    },
+    components:{
+      Character
+    }
+}
+</script>
